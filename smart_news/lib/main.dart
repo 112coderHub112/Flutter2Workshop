c1b4189113a0520e8d63f6c059fc4b941dc8c3bb
Kritsaphong Phuthibpaphaisit <kritsaphong.phu@stu.nida.ac.th>
2022-03-11 02:30:12 +0700

    add color and theme

--- smart_news/lib/main.dart
+++ smart_news/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smart_news/Themes/app_theme.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,7 +15,19 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Home'),
+      theme: AppTheme.lightTheme.copyWith(
+       //scaffoldBackgroundColor: Theme.of(context).cardTheme.color
+      ),
+
+
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Home'),
+        ),
+        body: Center(
+          child: Text('Hello flutter')
+          ),      
+      )
     );
   }
 }
